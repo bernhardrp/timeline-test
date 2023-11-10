9ea0f147ffcf2187e4846fca3ce1ad900b1720d2
bernhardrp <b.putranto@datatactics.de>
2023-11-10 09:11:59 +0100

    Implement manine progress bar timeline

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import { Progress } from '@mantine/core';
+
+export function ProgressBar () {
+  let numbers = [35, 25, 15, 25];
+  return (
+    <Progress.Root size="50">
+      <Progress.Section value={numbers[0]} color="gray" style={{borderColor:"gray", borderStyle: 'solid'}}>
+        <Progress.Label>Deboarding</Progress.Label>
+      </Progress.Section>
+      <Progress.Section value={numbers[1]} color="gray" style={{borderColor:"gray", borderStyle: 'solid'}}>
+        <Progress.Label>Cleaning</Progress.Label>
+      </Progress.Section>
+      <Progress.Section value={numbers[2]} color="gray" style={{borderColor:"gray", borderStyle: 'solid'}}>
+        <Progress.Label>Tanking</Progress.Label>
+      </Progress.Section>
+      <Progress.Section value={numbers[3]} color="gray" style={{borderColor:"gray", borderStyle: 'solid'}}>
+        <Progress.Label>Inspection</Progress.Label>
+      </Progress.Section>
+    </Progress.Root>
+  );
+}
--- src/pages/TimelinePage.tsx
+++ src/pages/TimelinePage.tsx
@@ -1,6 +1,51 @@
-
+import { Flex, Progress } from "@mantine/core";
+import { ProgressBar } from "../components/ProgressBar";
 export default function TimelinePage() {
     return(
-        <h1>TIMELINE</h1>
+        <>
+            <h1 style={{fontSize: '2em'}}>Mantine Progress Bar</h1>
+            <ul style={{listStyleType: "none", padding: 0, margin: 0}}>
+                <li>
+                    <Flex direction={"row"} justify={"flex-start"} align={"center"}>
+                        <h2 style={{width: 150, textAlign: "start"}}>Planned</h2>
+                        <div style={{minWidth: "100vh"}}>
+                            <ProgressBar/>
+                        </div>
+                    </Flex>
+                </li>
+                <li>
+                    <Flex align={"center"}>
+                        <h2 style={{width: 150, textAlign: "start"}}>Deboarding</h2>
+                        <div style={{width: '35vh'}}>
+                            <Progress size="25" value={100} />
+                        </div>
+                    </Flex>
+                </li>
+                <li>
+                    <Flex align={"center"}>
+                        <h2 style={{width: 150, textAlign: "start"}}>Cleaning</h2>
+                        <div style={{width: '25vh', marginLeft: '35vh'}}>
+                            <Progress size="25" value={100} />
+                        </div>
+                    </Flex>
+                </li>
+                <li>
+                    <Flex align={"center"}>
+                        <h2 style={{width: 150, textAlign: "start"}}>Tanking</h2>
+                        <div style={{width: '9vh', marginLeft: '60vh'}}>
+                            <Progress size="25" value={100} animated />
+                        </div>
+                    </Flex>
+                </li>
+                <li>
+                    <Flex align={"center"}>
+                        <h2 style={{width: 150, textAlign: "start"}}>Inspection</h2>
+                        {/* <div style={{width: '35vh'}}>
+                            <Progress size="25" value={100} animated />
+                        </div> */}
+                    </Flex>
+                </li>
+            </ul>
+        </>
     );
 }
