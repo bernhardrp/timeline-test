fc1df5007c7fdf497d7c560fa417ae11f4cb372a
bernhardrp <b.putranto@datatactics.de>
2023-12-07 11:54:51 +0100

    Remove unused text

--- src/pages/CustomTimelinePage.tsx
+++ src/pages/CustomTimelinePage.tsx
@@ -2,7 +2,7 @@ import GanttChart from '../components/GanttChart';
 
 const tasks = [
   { name: 'Task 1', start: new Date('2023-11-13T08:00:00'), end: new Date('2023-11-13T12:00:00') },
-  // { name: 'Task 2', start: new Date('2023-11-14T12:00:00'), end: new Date('2023-11-14T13:00:00') },
+  { name: 'Task 2', start: new Date('2023-11-13T12:00:00'), end: new Date('2023-11-13T13:00:00') },
   // Add more tasks as needed
 ];
 
@@ -10,7 +10,6 @@ const CustomTimelinePage = () => {
     return (
         <>
             <h1>Custom Timeline</h1>
-            <p>this custom timeline is not finished</p>
             <GanttChart tasks={tasks} />
         </>
     )
