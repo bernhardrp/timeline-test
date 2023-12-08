60e30490fd7dc28969a8066849c6729fa5962e9e
bernhardrp <b.putranto@datatactics.de>
2023-12-08 12:18:17 +0100

    Add progress attribute

--- src/pages/CustomTimelinePage.tsx
+++ src/pages/CustomTimelinePage.tsx
@@ -1,8 +1,8 @@
 import GanttChart from '../components/GanttChart';
 
 const tasks = [
-  { category: 'Planned', name: 'Task 1', start: new Date('2023-11-13T08:00:00'), end: new Date('2023-11-13T12:00:00') },
-  { category: 'Actual', name: 'Task 1', start: new Date('2023-11-13T08:00:00'), end: new Date('2023-11-13T11:00:00') },
+  { category: 'Planned', name: 'Task 1', start: new Date('2023-11-13T08:00:00'), end: new Date('2023-11-13T12:00:00'), progress: 100 },
+  { category: 'Actual', name: 'Task 1', start: new Date('2023-11-13T08:00:00'), end: new Date('2023-11-13T11:00:00'), progress: 90 },
   // Add more tasks as needed
 ];
 
